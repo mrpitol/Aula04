6901bd7192ff3d62d0ce67f56d3e9e2d68adefaa
Marcelo Pitol <mrpitol@outlook.com>
2021-06-12 15:06:52 -0300

    Teste com LoopWhile

--- src/LoopWhile.java
+++ src/LoopWhile.java
@@ -4,7 +4,11 @@ public class LoopWhile {
 	public static void main(String[] args) {
 		/*Imagine que uma ONG precise saber a média de idades dos alunos de uma escola.
 		 * Sabendo de antemão quantos alunos existem e sem a necessidade de armazenar permanentemente a idade de cada um desses alunos, qual é a melhor forma de calcular essa média?*/
-
+		int contador=0;
+		while(contador<10) {
+			System.out.println("Mensagem");
+			contador= contador+1;
+		}
 	}
 
 }
