7b9fe2706323b9f43a6dffa5e5af7acc2b4a2fd9
Marcelo Pitol <mrpitol@outlook.com>
2021-06-12 16:16:26 -0300

    Correção do Exercicio 3

--- src/Exercicios3Aula4.java
+++ src/Exercicios3Aula4.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class Exercicios3Aula4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Crie um programa capaz de calcular a tabuada de um número digitado pelo usuário.
+	
+	Scanner leitor= new Scanner(System.in);
+	int n,res;
+	int contador=1;
+	System.out.println("Você quer calcular a tabuada de qual número?");
+	n=leitor.nextInt();
+	System.out.println("");
+	System.out.println("Calculando a tabuada do " + n);
+	while (contador<=10) {
+		res = n * contador;
+		System.out.println(n+ "x" + contador + " = " + res);
+		contador =contador+1;
 	}
-
+	System.out.println("");
+	System.out.println("Valores da tabuada do " + n);
+	leitor.close();
+	}
 }
